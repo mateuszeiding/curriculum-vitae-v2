5e8094ac3c0ac7e8a61884a17e08165ecf936e6a
MATEUSZ EIDING <eidingmateusz@gmail.com>
2025-10-13 20:31:51 +0200

    remove unused data

--- src/server/data/getCertificates.mock.ts
+++
@@ -1,9 +0,0 @@
-import type { CertificateDto } from "@models/Certificate.dto";
-
-export const getCertificates: CertificateDto[] = [
-	{
-		name: "Professional Scrum Master I (PSM I)",
-		link: "https://www.credly.com/badges/175db99c-c1be-4ecd-a6c4-e6d8f4738d55",
-		startDate: new Date("2022-05-01"),
-	},
-];
